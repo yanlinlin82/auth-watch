24098768ca3fa3e3c9620205e7bee1735c08063c
Linlin Yan <yanlinlin82@gmail.com>
2016-07-10 01:13:56 +0800

    Resolve domain name to IP address

--- firewall.sh
+++ firewall.sh
@@ -4,6 +4,10 @@ LIST_DIR="$(dirname $0)/ban-ip"
 
 iptables -F BAN-IP
 
+IPV4_ADDR_PATTERN='[0-9][0-9]*\.[0-9][0-9]*\.[0-9][0-9]*\.[0-9][0-9]*'
 cat ${LIST_DIR}/*.txt | while read IP; do
+	if !(echo ${IP} | grep -q "^${IPV4_ADDR_PATTERN}$"); then
+		IP=$(host ${IP} | sed -r 's/^.*[^0-9]('${IPV4_ADDR_PATTERN}').*$/\1/')
+	fi
 	iptables -A BAN-IP -s ${IP} -j DROP
 done
